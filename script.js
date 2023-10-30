6ac5d668cf46cbe6914d4434eef7b90a8fcff312
Hoàng Sơn <95064141+Akiyue@users.noreply.github.com>
2023-10-30 23:58:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+    autoScrolling:true,
+    navigation:true,
+    anchors: ['section1', 'section2', 'section3', 'section4', 'section5', 'section6'],
+    navigationTooltips: ['Teyvat', 'Mondstadt', 'Liyue', 'Inazuma', 'Sumeru', 'Fontaine'],
+    showActiveTooltip: true,
+    scrollingSpeed: 1000,
+});
